fa7bb12d061a17a8172f1da9ea92217f60b9cc8f
termite9 <ttermite0@gmail.com>
2023-12-01 20:58:25 +0900

    삭제복구

--- pylog/posts/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from posts.views import feeds
-urlpatterns = [
-    path("feeds/", feeds),
-]
--- pylog/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-# Create your models here.
-class User(AbstractUser):
-    profile_image = models.ImageField(
-        "프로필이미지", upload_to = "users/profile", blank=True)
-    short_description = models.TextField("소개글", blank=True)
